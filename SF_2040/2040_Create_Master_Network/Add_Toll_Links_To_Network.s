9fd85fc2068ae1a381dc961f257b6b4128a98b1e
Amar <amarsarvepalli19@gmail.com>
2017-11-28 11:23:05 -0500

    Create 2040 Master Network File
    Append all csv files to one network and write out Node, Link and Toll files with only required fields for c++ code to work.

---
+++ SF_2040/2040_Create_Master_Network/Add_Toll_Links_To_Network.s
@@ -0,0 +1,7 @@
+; Do not change filenames or add or remove FILEI/FILEO statements using an editor. Use Cube/Application Manager.
+RUN PGM=NETWORK
+FILEI LINKI[1] = "SF.net"
+FILEI LINKI[2] = "Toll_Link_All.dbf"
+FILEO NETO = "SF_TollLinks.net"
+
+ENDRUN
